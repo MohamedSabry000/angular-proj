d685c76921086aef1cb99b818af565f623dfadc2
Mohamed Sabry <30502846+Mohamedfadah@users.noreply.github.com>
2022-05-24 13:27:58 +0200

    page layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { HeaderContainerComponent } from './header-container/header-container.component';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavComponent,
+    HeaderContainerComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss','sidebar.css']
+})
+export class HomeComponent implements OnInit {
+
+  imgSrc?:string ;
+  peopleByCountry: any[] =
+  [ { 'country': 'UK', 'people':    [ { "name": "Douglas Pace" }, { "name": "Mcleod Mueller" }, ] },
+    { 'country': 'US', 'people':    [ { "name": "Day Meyers" }, { "name": "Aguirre Ellis" }, { "name": "Cook Tyson" } ] },
+    { 'country': 'MARS', 'people':  [ { "name": "Douglas Pace" }, { "name": "Mcleod Mueller" }, ] }
+  ];
+
+  constructor() {
+    this.imgSrc = "../../assets/man.jpg";
+   }
+
+  ngOnInit(): void {
+  }
+
+}
